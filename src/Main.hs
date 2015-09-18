801e1dd8f91129f9e0806f2378361ac102f0f1a4
Ye Yan <ye.yan@parashift.com.au>
2015-09-18 16:30:48 +0930

    Refactored parser to allow more time format

--- src/Main.hs
+++ src/Main.hs
@@ -5,28 +5,110 @@ import System.Environment
 import Control.Monad
 import Control.Applicative
 
-intervals records =
-    foldl merge [] $ zipWith (\(_, t1) (label, t2) -> (label, t2 - t1)) records $ tail records
+import Data.Ord
+import Data.Time
+import Data.List
 
-merge rs (label, time) = 
-    filter (\(l, _) -> l /= label) rs ++ [(label, time + maybe 0 id (lookup label rs))]
+--intervals records = 
+--    foldl merge [] $ zipWith (\(_, t1) (label, t2) -> (label, t2 - t1)) records $ tail records
+--
+--merge rs (label, time) = 
+--    filter (\(l, _) -> l /= label) rs ++ [(label, time + maybe 0 id (lookup label rs))]
+--
+--formatFloatN floatNum numOfDecimals = 
+--    showFFloat (Just numOfDecimals) floatNum ""
+--
+--printIntervals file []      = putStrLn "Not enough time record"
+--printIntervals file records = do
+--    let maxLabelLen = foldl1 max $ map (length . fst) records
+--        maxLineLen = maxLabelLen + 12
+--        printHorizontalLine = putStrLn $ replicate maxLineLen '='
+--    putStrLn file
+--    printHorizontalLine
+--    mapM_ (\(label, time) -> putStrLn $ "| " ++ label ++ replicate (maxLabelLen - length label) ' ' ++ " | " ++ formatFloatN time 2 ++ "h |") records
+--    printHorizontalLine
+--
+--main = do
+--    args <- getArgs
+--    forM_ args $ \file -> do
+--        result <- liftA intervals <$> parseTimeSheet file
+--        case result of
+--            Left error -> putStrLn $ "Error: " ++ show error
+--            Right records -> printIntervals file records
+--
 
-formatFloatN floatNum numOfDecimals = 
-    showFFloat (Just numOfDecimals) floatNum ""
+--merge rs (label, time) = 
+--    filter (\(l, _) -> l /= label) rs ++ [(label, time + maybe 0 id (lookup label rs))]
+
+intervals entries = 
+    sortBy (\(n1, _) (n2, _) -> compare n1 n2) $ foldl merge [] $ 
+        zipWith (\(t1, _) (t2, name) -> (name, t2 - t1)) records $ tail records
+    where
+        records = 
+            map (\(time, name) -> (timeOfDayToTime (localTimeOfDay time), name)) entries
+        merge rs (label, time) = 
+            filter (\(l, _) -> l /= label) rs ++ [(label, time + maybe (secondsToDiffTime 0) id (lookup label rs))]
+
+printTable rows = do
+    let colWidths  = map (foldl1 max) $ transpose $ map (map length) rows
+        tableWidth = length colWidths * 3 + 1 + sum colWidths
+        cell (len, msg) = " " ++ msg ++ " " ++ replicate (len - length msg) ' ' 
+        seperator = putStrLn $ replicate tableWidth '='
+    seperator
+    mapM_ putStrLn $ map ( (++ "|") . ((++) "|") . concat . intersperse "|") $ map (map cell) $ map (zip colWidths) rows 
+    seperator
+
+tableColumnSizes :: [[String]] -> [Int]
+tableColumnSizes = map (foldl1 max) . transpose . map (map length)
+
+alignL (size, text) = replicate (size - length text) ' ' ++ text ++ " "
+alignR (size, text) = " " ++ text ++ replicate (size - length text) ' '
+alignC (size, text) = 
+    let p = size - length text
+        l = floor $ toRational p / 2
+        r = p - l
+    in replicate l ' ' ++ text ++ replicate r ' '
+
+tableRow = map ((++ "|") . ((++) "|") . concat . intersperse "|")
+tableSeperator size = replicate size '='
+
+printT table = do
+    let colWidths  = map (foldl1 max) $ transpose $ map (map length) $ concat $ map snd table
+        tableWidth = length colWidths * 3 + 1 + sum colWidths
+        seperator  = putStrLn $ tableSeperator tableWidth
+        cell (len, msg) = " " ++ msg ++ " " ++ replicate (len - length msg) ' ' 
+    seperator
+    forM_ table $ \(day, intervals) -> do
+        putStrLn $ (++ "|") $ ((++) "|") $alignC (tableWidth - 2, (show day))
+        mapM_ putStrLn $ map ( (++ "|") . ((++) "|") . concat . intersperse "|") $ map (map cell) $ map (zip colWidths) intervals
+        seperator
 
 main = do
     args <- getArgs
     forM_ args $ \file -> do
-        result <- liftA intervals <$> parseTimeSheet file
-        putStrLn $ show result
+        result <- parseTimeSheet file
         case result of
             Left error -> putStrLn $ "Error: " ++ show error
-            Right records -> do
-                let maxLabelLen = foldl1 max $ map (length . fst) records
-                    maxLineLen = maxLabelLen + 12
-                    printHorizontalLine = putStrLn $ replicate maxLineLen '='
-                putStrLn $ "* " ++ file
-                printHorizontalLine
-                mapM_ (\(label, time) -> putStrLn $ "| " ++ label ++ replicate (maxLabelLen - length label) ' ' ++ " | " ++ formatFloatN time 2 ++ "h |") records
-                printHorizontalLine
+            Right entries -> do --printIntervals file records
+                let showInHour secs = showFFloat (Just 2) (secs / 3600) "h"
+                    day = localDay . fst
+                    present = map $ \(name, time) -> [name, showInHour (fromRational (toRational time))]
+                    process = map (\group -> ((day . head) group, (present . intervals) group)) . groupBy (\e1 e2 -> day e1 == day e2)
+                    results = process entries
+                printT results
+
+test = do
+    result <- parseTimeSheet "test.txt"
+    let Right entries = result
+        showInHour secs = showFFloat (Just 2) (secs / 3600) "h"
+
+        day = localDay . fst
+        present = map $ \(name, time) -> [name, showInHour (fromRational (toRational time))]
+        process = map (\group -> ((day . head) group, (present . intervals) group)) . groupBy (\e1 e2 -> day e1 == day e2)
+        results = process entries
+    printT results
+    --putStrLn $ show $ groupBy (\e1 e2 -> localDay (fst e1) == localDay (fst e2)) entries
+    --return $ map intervals $ groupBy (\e1 e2 -> localDay (fst e1) == localDay (fst e2)) entries
+    --x <- return $ map (\(name, time) -> [name, showInHour (fromRational (toRational time))]) $ map intervals entries
+    --printTable x
 
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,23 +1,95 @@
 module Parser (parseTimeSheet) where
 
-import Text.Parsec
-import Text.Parsec.String
+import Text.Parsec hiding (space, spaces)
+import Control.Monad.IO.Class
+import Control.Applicative (liftA)
 
-time :: Parser Double
-time = do
-    h <- many1 digit
-    char ':'
-    m <- many1 digit
-    return $ read h + (read m) / 60
+import Data.Char (isSpace)
+import Data.Time
 
-line :: Parser (String, Double)
-line = do
+type Parser = ParsecT String () IO
+
+space :: Parser Char
+space = do
+    oneOf " \t"
+
+spaces :: Parser [Char]
+spaces = do
     many space
-    t <- time <?> "time"
-    many $ oneOf " "
-    l <- option "" $ many1 $ noneOf "\n\r"
-    endOfLine
-    return (l, t)
 
-parseTimeSheet fileName = do
-    parseFromFile (many line) fileName
+basicDate :: Parser Day
+basicDate = do
+    y <- count 4 digit
+    char '-'
+    m <- number
+    char '-'
+    d <- number
+    return $ fromGregorian (read y) m d
+    where
+        number = do
+            value <- try (count 2 digit) <|> count 1 digit
+            return $ read value
+
+basicTime :: Parser TimeOfDay
+basicTime = do
+    h <- number
+    char ':'
+    m <- number
+    s <- option 0 $ char ':' >> number
+    return $ TimeOfDay h m $ fromIntegral s
+    where
+        number :: Parser Int
+        number = do
+            value <- try (count 2 digit) <|> count 1 digit   
+            return $ read value
+
+dateTime = do
+    d <- basicDate
+    space <|> char 'T'
+    t <- basicTime
+    return $ LocalTime d t
+
+time :: Parser LocalTime
+time = do
+    d <- liftIO $ do
+        zone <- getCurrentTimeZone
+        curr <- getCurrentTime
+        return $ localDay $ utcToLocalTime zone curr
+    t <- basicTime
+    return $ LocalTime d t
+
+entryTime =
+    try dateTime <|> try time <?>
+        "Expecting timestamp in the format of \"yyyy-M[M]-d[d] h[h]:m[m]:s[s]\" \"h[h]:m[m]:s[s]\" or \"h[h]:m[m]\""
+
+entryName = do
+    input <- many $ noneOf "\n\r#"
+    return $ strip input
+    where
+        stripL = dropWhile isSpace
+        stripR = reverse . stripL . reverse
+        strip  = stripR . stripL
+
+comment = do
+    char '#'
+    many $ noneOf "\n\r"
+
+entry = do
+    t <- entryTime
+    n <- option "" $ spaces >> entryName
+    optional comment
+    endOfLine
+    return [(t, n)]
+
+empty :: Parser [(LocalTime, String)]
+empty = do
+    try comment <|> spaces
+    endOfLine
+    return []
+
+line = do
+    try entry <|> try empty <?> "Expecting entry or comment or empty line"
+
+parseTimeSheet fname = do
+    content <- readFile fname
+    liftA (liftA concat) $ runParserT (many line) () fname content
